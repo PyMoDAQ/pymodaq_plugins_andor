f3bfe645cf2922945ee6c5a87950e2f433d96265
Sébastien Weber <seba.weber@gmail.com>
2020-11-09 15:23:55 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,11 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'andor'  #for instance daqmx
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_andor' #to modify
+description = 'Set of PyMoDAQ plugins for Andor Camera (CCD camera using SDK2, SCMOS cameras using SDK3...)'
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
+
+#packages required for your plugin:
+packages_required = []
+##
